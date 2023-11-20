a9af833e3ea67a0f3744eecfc69b82828ea0dbea
julia Wager <julia@p-wagner.at>
2023-11-20 21:12:10 +0100

    create a model with the migration

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# after creating a new model run the following to make migrations:
+# python manage.py makemigrations (--dry-run to preview)
+# python manage.py showmigrations (to show migrations)
+# python manage.py migrate (--plan to preview)
+# register in models.py
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # update name showing in admin
+    def __str__(self):
+        return self.name
